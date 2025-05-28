4b0a23fd5cefe214598deed90bb360aeec688597
xoxown <xoxown31@gmail.com>
2025-05-28 20:09:45 +0900

    [feat] Implement SearchFlightsCommand and update CustomerState to include it

--- src/com/aport/state/CustomerState.java
+++ src/com/aport/state/CustomerState.java
@@ -9,29 +9,31 @@ public class CustomerState extends AbstractUserState {
     @Override
     public void initializeCommands() {
         commands.put(1, new ViewFlightsCommand());
-        commands.put(2, new CreateReservationCommand());
-        commands.put(3, new ModifyReservationCommand());
-        commands.put(4, new CancelReservationCommand());
-        commands.put(5, new ViewReservationsCommand());
-        commands.put(6, new PaymentCommand());
-        commands.put(7, new LogoutCommand());
+        commands.put(2, new SearchFlightsCommand());
+        commands.put(3, new CreateReservationCommand());
+        commands.put(4, new ModifyReservationCommand());
+        commands.put(5, new CancelReservationCommand());
+        commands.put(6, new ViewReservationsCommand());
+        commands.put(7, new PaymentCommand());
+        commands.put(8, new LogoutCommand());
     }
 
 
     @Override
     public void displayMenu() {
-        System.out.println("1. 항공편 검색/선택");
-        System.out.println("2. 예약 생성");
-        System.out.println("3. 예약 수정");
-        System.out.println("4. 예약 삭제");
-        System.out.println("5. 내 예약 조회");
-        System.out.println("6. 티켓 발권");
-        System.out.println("7. 로그아웃");
+        System.out.println("1. 항공편 조회");
+        System.out.println("2. 항공편 검색");
+        System.out.println("3. 예약 생성");
+        System.out.println("4. 예약 수정");
+        System.out.println("5. 예약 삭제");
+        System.out.println("6. 내 예약 조회");
+        System.out.println("7. 티켓 발권");
+        System.out.println("8. 로그아웃");
     }
 
     @Override
     protected boolean isExitChoice(int choice) {
-        return choice == 7;
+        return choice == 8;
     }
 
     @Override
